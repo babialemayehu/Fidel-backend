c13b95fbfa3302aacc97e3d330d0a984b6f9c2e9
eba alemayehu <eba.alemayehu100@gmail.com>
2018-11-07 17:09:39 +0300

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
          <div class="carousel-inner" role="listbox">
             <div class="item active">
                 <div class="carousel-caption" style="margin-top: -5%">
-                    <h1 class='text-center' style="font-family: 'Cookie', cursive; font-size: 6em" >{!! $websiteTitle !!}</h1>
+                    <h1 class='text-center' style="font-family: 'Cookie', cursive; font-size: 6em" >rtert {!! $websiteTitle !!}</h1>
                     <p class='text-center'>Make your life easier witdsafaf fidel learning managment</p>
                     <P><a href="#login"><button class="btn btn-success btn-lg" >Log in</button></a>
                         <!-- <button class='btn btn-default btn-lg' data-toggle='modal' data-target='#signup-modal'>Sign up</button> -->
