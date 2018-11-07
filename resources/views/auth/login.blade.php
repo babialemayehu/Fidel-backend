db7d67826e07382a40f0bed199ab5dd3cc5cb6c2
eba alemayehu <eba.alemayehu100@gmail.com>
2018-11-07 18:45:49 +0300

    ghsdfdd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.login')
 @section("content")
+<h1>lkaflkajjsdfjasdokdfopiasdjfoasdfoiasdoijawe oiwawjafwaopjjf</h1>
     <div id="login" class="carousel slide" data-ride="carousel">
          <div class="carousel-inner" role="listbox">
             <div class="item active">
                 <div class="carousel-caption" style="margin-top: -5%">
                     <h1 class='text-center' style="font-family: 'Cookie', cursive; font-size: 6em" > {!! $websiteTitle !!}</h1>
-                    <p class='text-center'>Make your life easier witdsafaf fidel learning managment</p>
+                    <p class='text-center'>Make your life easier with</p>
                     <P><a href="#login"><button class="btn btn-success btn-lg" >Log in</button></a>
                         <!-- <button class='btn btn-default btn-lg' data-toggle='modal' data-target='#signup-modal'>Sign up</button> -->
                     </p>
