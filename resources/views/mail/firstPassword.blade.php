062fab5e0e571e679cf301beb52a7049763a1116
eba alemayehu <eba.alemayehu100@gmail.com>
2018-11-08 14:59:42 +0300

    email page produced

--- resources/views/mail/firstPassword.blade.php
+++ resources/views/mail/firstPassword.blade.php
@@ -1,69 +1,62 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie" /> 
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wellcome</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Lobster" rel="stylesheet">
+    <style>
+            html,body{
+            font-family: 'Roboto', sans-serif;
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Fidel') }}</title>
+        }
+        .cursive-marker{font-family: 'Lobster', cursive;}
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/layout.css')}}" />
-    <link rel="stylesheet" href="{{ asset('/css/custom.bootstrap.css') }}" />
+        nav>div>span{
+            margin-top: 24px; 
+            color: white; 
+            font-size: 24px;
+        }
+        button {
+            padding: 12px 16px; 
+            font-size: 16px;  
+            color: white; 
+            border: none; 
+            background-color: rgba(27,157,116,1);
+            box-shadow: 1px 2px 5px rgba(0,0,0,0.3); 
+            border-radius: 3px; 
+        }
+        button:hover{  
+            background-color: rgba(27,157,116,1);
 
+        }
+    </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top" style="background-color:rgba(27, 157, 116, 1);">
-            <div class="container">
-                <div class="navbar-header">
-                    <a href="URL::to('/#/')" class="navbar-brand" style="color:white;font-size:1.6em;padding:6px" >
-                        <img src="{{ asset('/img/logo-inverse.png') }}" style="width:25px;display:inline"> 
-                        <span>{{str_replace('S','',$websiteTitle)}}</span>
-                    </a>
-                </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="row">
-                <h3>Welcome to fidel</h3>
-                <p>Fidel is a learning managment tool which is developed to facilitate the communication 
-                    between teacher and students. </p>
-                <p>
-                    With fidel you can
-                </p>
-                <ul>
-                    <li>Share files</li>
-                    <li>Check every events</li>
-                    <li>Submit assignments</li>
-                    <li>View results</li>
-                </ul>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <h5>This is your temporary password. Use your registral id as username; 
-                <h6 style="text-align: center;">{{$password}}</h6>
-                <div style="text-align: center;" class="container">
-                    <a href="{{ URL::to('/login#login') }}">
-                        <button class="btn btn-success">LOGIN</button>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <main>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+        <div style="text-align: center; margin-top: 80px" class="container">
+            <img src="https://doc-0c-00-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/1v7rj6ikk0khf8tvmmki7ivl61f1lg5s/1541671200000/07635287236685665338/*/1J_rROhJjeCKJlsLyfgRO_KcXmkPv3wK_">     
+        </div> 
+        <div style="text-align: center;font-size: 48px; margin-top: 8px" class="cursive-marker">Welcome to fidel</div>
+        <div style="text-align: center">
+            <p>This is your temporary password. Please use your registral id as user name and login.</p>
+            <div class="password" style="margin: 24px;padding: 8px 16px; font-size: 14px; background-color: rgba(0,0,0,0.1); display:inline-block">
+                <strong>{{$password}}</strong>
+            </div>
+            <div>
+                <a href="">
+                    <button>LOG IN</button>
+                </a>
+            </div>
+            
+        </div>
+        
+    </main>
+            
+
 </body>
 </html>
