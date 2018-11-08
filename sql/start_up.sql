43edbb3fec131012deaf4247855cd6b993878e33
eba alemayehu <eba.alemayehu100@gmail.com>
2018-11-08 11:24:59 +0300

    next day deploymment

--- sql/start_up.sql
+++ sql/start_up.sql
@@ -3,3 +3,10 @@ VALUES
 (NULL, 'Student', NULL, NULL), 
 (NULL, 'Teacher', NULL, NULL), 
 (NULL, 'Department head', NULL, NULL);
+
+INSERT INTO `departments` (`id`, `user_id`, `collage_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'Software engineering', NULL, NULL);
+
+
+INSERT INTO `collages` (`id`, `user_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Technology', NULL, NULL);
