a5dcfa6c8028d641bac6c0f09dab4be3b0f802a3
eba alemayehu <eba.alemayehu100@gmail.com>
2018-11-17 21:48:41 +0300

    jdksf

--- app/Http/Controllers/chapterController.php
+++ app/Http/Controllers/chapterController.php
@@ -7,43 +7,24 @@ use Illuminate\Http\Request;
 
 class chapterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
